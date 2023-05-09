a77c8e8cd0d2ff1f27f9572743ef6b6d2ec1ec84
akinyiliz <lizzieke84@gmail.com>
2023-05-09 22:12:38 +0300

    Created & styled bg

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,17 @@ import "./App.css";
 
 function App() {
 	return (
-		<>
-			<div className="font-bold">Hello</div>
-		</>
+		<div className="w-full h-screen relative app">
+			<div className="max-w-[980px] absolute mt-12 mx-auto">
+				<div className="">
+					<input
+						type="text"
+						placeholder="Enter a city..."
+						className=""
+					/>
+				</div>
+			</div>
+		</div>
 	);
 }
 
