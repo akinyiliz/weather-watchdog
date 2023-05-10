9eaaec4c9f611cb19a4866e5ba5bf9e2f8226be2
akinyiliz <lizzieke84@gmail.com>
2023-05-10 09:49:27 +0300

    Added inpu element and main component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@ import "./App.css";
 
 function App() {
 	return (
-		<div className="w-full h-screen relative app">
-			<div className="max-w-[980px] absolute mt-12 mx-auto">
-				<div className="">
+		<div className="w-full h-screen app">
+			<div className="max-w-[1240px] mx-auto p-12">
+				<div className="max-w-[500px] mx-auto">
 					<input
 						type="text"
 						placeholder="Enter a city..."
-						className=""
+						className="w-full p-2 rounded-lg text-lg"
 					/>
+				</div>
+
+				<div className="max-w-[900px] w-full mx-auto h-[400px] rounded-2xl mt-12 bg-white/10">
+					<h1>Weather</h1>
+				</div>
+				<div className="flex items-center justify-between max-w-[900px] mx-auto mt-[-4]">
+					<div className="w-[200px] h-[150px] bg-black/10">Weather 1</div>
+					<div className="w-[200px] h-[150px] bg-black/10">Weather 1</div>
+					<div className="w-[200px] h-[150px] bg-black/10">Weather 1</div>
 				</div>
 			</div>
 		</div>
